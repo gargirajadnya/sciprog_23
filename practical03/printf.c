927f4a55380c102cc09dcfa813ecc7f87f36b9be
Gargi <gargirajadnya@gmail.com>
2023-10-11 20:11:27 +0100

    Practical03 initial commit

---
+++ practical03/printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    //declaring 2 integers and 2 float variables
+    int i1 = 10;
+    int i2 = 200;
+    float f1 = 20.1;
+    float f2 = 2.22459;
+
+    //printing the four variables
+    printf("Two integers: %d, %d \nTwo floats: %.1f, %.3f\n", i1, i2, f1, f2);
+
+    return 0;
+
+}
