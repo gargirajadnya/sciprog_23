4698202d36a16741917dad564f79ac3559986875
Gargi <gargirajadnya@gmail.com>
2023-11-01 08:40:15 +0000

    practical 06 initial commit

---
+++ practical06/Step6/matmult.c
@@ -0,0 +1,19 @@
+#include "matmult.h"
+#include<stdio.h>
+
+void matmult(int n, int p, int q, double A[n][p], double B[p][q],double C[n][q]) {
+    //Loop indices
+    int i, j, k; 
+
+    //Matrix multiplication performed
+    for (i=0; i<n; i++)
+    { 
+    	for(j=0; j<q; j++)
+	    { 
+    		for (k=0; k<p; k++)
+    		{ 
+    	        C[i][j]=C[i][j]+A[i][k]*B[k][j];
+            }
+        }
+    }
+}
---
+++ practical06/Step6/matmult.h
@@ -0,0 +1,6 @@
+#ifndef MATMULT_H
+#define MATMULT_H
+
+void matmult(int n, int p, int q, double A[n][p], double B[p][q], double C[n][q]);
+
+#endif /* MATMULT_H */
